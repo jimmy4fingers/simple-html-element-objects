ca3c8ea9f8d23074d312c7c3f86c8bce4c1e82eb
James Lee <james.lee@datamatters.co.uk>
2018-08-23 10:46:03 +0100

    pre-commit test

--- composer-post-install.php
+++ composer-post-install.php
@@ -4,5 +4,10 @@
  */
 echo "Running post composer install scripts..." . PHP_EOL;
 
+// create logs folder
+if (!file_exists('logs')) {
+    mkdir('logs', 0777, true);
+}
+
 // set up git hooks
 include_once('composer'.DIRECTORY_SEPARATOR.'scripts'.DIRECTORY_SEPARATOR.'git'.DIRECTORY_SEPARATOR.'git-hooks.php');
